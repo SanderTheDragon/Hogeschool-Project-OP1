a1c974fe5f42dabd60c2ffdcbe8f3f8ee29ffd3f
SanderTheDragon <sanderthedragon@zoho.com>
2016-09-09 11:56:57 +0200

    Code van vandaag toegevoegd

---
+++ sketch_sep09a.ino
@@ -0,0 +1,53 @@
+const int numbers[11][7] = {
+  { 1, 1, 1, 1, 1, 1, 0 },
+  { 0, 1, 1, 0, 0, 0, 0 },
+  { 1, 1, 0, 1, 1, 0, 1 },
+  { 1, 1, 1, 1, 0, 0, 1 },
+  { 0, 1, 1, 0, 0, 1, 1 },
+  { 1, 0, 1, 1, 0, 1, 1 },
+  { 1, 0, 1, 1, 1, 1, 1 },
+  { 1, 1, 1, 0, 0, 0, 0 },
+  { 1, 1, 1, 1, 1, 1, 1 },
+  { 1, 1, 1, 1, 0, 1, 1 },
+  { 0, 0, 0, 0, 0, 0, 0 }
+};
+
+const int segments[7] = { 9, 2, 3, 5, 6, 8, 7 };
+
+const int digits[4] = { 10, 11, 12 ,13 };
+
+void setup()
+{
+  for (int i = 2; i <= 13; i++)
+    pinMode(i, OUTPUT);
+    
+  digitalWrite(digits[0], HIGH);
+  digitalWrite(digits[1], HIGH);
+  digitalWrite(digits[2], HIGH);
+  digitalWrite(digits[3], HIGH);
+}
+
+void loop()
+{
+  const int n[4] = { 1, 2, 3, 4 };
+  showDigits(n);
+}
+
+void showDigits(const int* num)
+{
+  for (int i = 0; i < 4; i++)
+  {
+    showDigit(numbers[num[i]], digits[i]);
+    delay(1000);
+  }
+}
+
+void showDigit(const int* num, const int dig)
+{
+  digitalWrite(dig, LOW);
+  
+  for (int i = 0; i <= 7; i++)
+  {
+    digitalWrite(segments[i], num[i]);
+  }
+}
