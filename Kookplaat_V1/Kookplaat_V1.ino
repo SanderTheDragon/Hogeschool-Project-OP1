1d18ccf16d314e27ec2adee00917d7bc5b9cebd3
SanderTheDragon <sanderthedragon@zoho.com>
2016-10-11 09:53:47 +0200

    Updated, hopelijk werkt het

--- Kookplaat_V1/Kookplaat_V1.ino
+++ Kookplaat_V1/Kookplaat_V1.ino
@@ -42,68 +42,52 @@ const int clockPin = 8;
 const int dataPin  = 12;
 const int latchPin = 13;
 
-const int disp[8] = { 0, 1, 2, 3, 5, 6, 9, 10 };
+const int displayPin[8] = { 0, 1, 2, 3, 5, 6, 9, 10 };
 
-const int button1 = 4;    //Pit omhoog
-const int button2 = 7;    //Temperatuur omhoog
-const int button3 = A4;   //Aan
-const int button4 = A5;   //Kinderslot aan
+const int beeperPin = 11;
 
-const int button4m = A3;  //Pit omlaag
-const int button3m = A2;  //Temperatuur omlaag
-const int button2m = A1;  //Uit
-const int button1m = A0;  //Kinderslot uit
+const int buttonPitUp = 4;     //Pit omhoog
+const int buttonTempUp = 7;    //Temperatuur omhoog
+const int buttonOn = A4;       //Aan
+const int buttonSafeOn = A5;   //Kinderslot aan
 
-const int beeper = 11;
-
-/*const int led1 = 5;
-const int led2 = 6;
-const int led3 = 9;
-const int led4 = 10;*/
+const int buttonPitDown = A3;  //Pit omlaag
+const int buttonTempDown = A2; //Temperatuur omlaag
+const int buttonOff = A1;      //Uit
+const int buttonSafeOff = A0;  //Kinderslot uit
 
 void setup()
 {
   pinMode(latchPin, OUTPUT);
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin,  OUTPUT);
+  
+  for (int displayIndex = 0; displayIndex < 8; displayIndex++)
+    pinMode(displayPin[displayIndex], OUTPUT);
 
-  pinMode(disp1, OUTPUT);
-  pinMode(disp2, OUTPUT);
-  pinMode(disp3, OUTPUT);
-  pinMode(disp4, OUTPUT);
-  pinMode(disp5, OUTPUT);
-  pinMode(disp6, OUTPUT);
-  pinMode(disp7, OUTPUT);
-  pinMode(disp8, OUTPUT);
+  pinMode(buttonPitUp, INPUT);
+  pinMode(buttonTempUp, INPUT);
+  pinMode(buttonOn, INPUT);
+  pinMode(buttonSafeOn, INPUT);
 
-  pinMode(button1, INPUT);
-  pinMode(button2, INPUT);
-  pinMode(button3, INPUT);
-  pinMode(button4, INPUT);
+  pinMode(buttonPitDown, INPUT);
+  pinMode(buttonTempDown, INPUT);
+  pinMode(buttonOff, INPUT);
+  pinMode(buttonSafeOff, INPUT);
 
-  pinMode(button1m, INPUT);
-  pinMode(button2m, INPUT);
-  pinMode(button3m, INPUT);
-  pinMode(button4m, INPUT);
-
-  pinMode(beeper, OUTPUT);
-
-  /*pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
-  pinMode(led4, OUTPUT);*/
+  pinMode(beeperPin, OUTPUT);
 }
 
-int i[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+int temp[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
 
-int i1t = 0;
-int i2t = 0;
-int i3t = 0;
-int i4t = 0;
-int i1tm = 0;
-int i2tm = 0;
-int i3tm = 0;
-int i4tm = 0;
+int bPitUp = 0;
+int bPitDown = 0;
+int bTempUp = 0;
+int bTempDown = 0;
+int bOn = 0;
+int bOff = 0;
+int bSafeOn = 0;
+int bSafeOff = 0;
 
 int pit = 0;
 
@@ -128,7 +112,6 @@ void loop()
     }
     
     DisplayNumber();
-    Led();
   }
   else
   {
@@ -137,221 +120,97 @@ void loop()
   }
 }
 
-void DisplayDigit(int disp, byte digit)
+void DisplayDigit(int displayIndex, byte digitL, byte digitR)
 {
   digitalWrite(latchPin, LOW);
   DisplayOff();
 
-  shiftOut(dataPin, clockPin, LSBFIRST, digit);
+  shiftOut(dataPin, clockPin, LSBFIRST, digitL);
+  shiftOut(dataPin, clockPin, LSBFIRST, digitR);
 
   digitalWrite(latchPin, HIGH);
-  digitalWrite(disp, LOW);
+  
+  if (displayIndex < 4)
+  {
+    digitalWrite(displayPin[displayIndex], LOW);
+    digitalWrite(displayPin[displayIndex + 4], LOW);
+  }
+  else
+  {
+    digitalWrite(displayPin[displayIndex - 4], LOW);
+    digitalWrite(displayPin[displayIndex], LOW);
+  }
+  
   delay(1);
 }
 
 void DisplayOff()
 {
-  digitalWrite(disp[0], HIGH);
-  digitalWrite(disp[1], HIGH);
-  digitalWrite(disp[2], HIGH);
-  digitalWrite(disp[3], HIGH);
-  digitalWrite(disp[4], HIGH);
-  digitalWrite(disp[5], HIGH);
-  digitalWrite(disp[6], HIGH);
-  digitalWrite(disp[7], HIGH);
+  for (int displayIndex = 0; displayIndex < 8; displayIndex++)
+    digitalWrite(displayPin[displayIndex], HIGH);
 }
 
 void DisplayNumber()
 {
-  if (pit == 0)
-    DisplayDigit(disp[0], byte(dotDigits[i[0]]));
-  else
-    DisplayDigit(disp[0], byte(digits[i[0]]));
-
-  if (pit == 1)
-    DisplayDigit(disp2, byte(dotDigits[i2]));
-  else
-    DisplayDigit(disp2, byte(digits[i2]));
-
-  if (pit == 2)
-    DisplayDigit(disp3, byte(dotDigits[i3]));
-  else
-    DisplayDigit(disp3, byte(digits[i3]));
-
-  if (pit == 3)
-    DisplayDigit(disp4, byte(dotDigits[i4]));
-  else
-    DisplayDigit(disp4, byte(digits[i4]));
+    DisplayDigit(0, byte(digits[temp[0]]), byte(digits[temp[4]]));
+    DisplayDigit(1, byte(digits[temp[1]]), byte(digits[temp[5]]));
+    DisplayDigit(2, byte(digits[temp[2]]), byte(digits[temp[6]]));
+    DisplayDigit(3, byte(digits[temp[3]]), byte(digits[temp[7]]));
     
-  if (pit == 4)
-    DisplayDigit(disp5, byte(dotDigits[i5]));
-  else
-    DisplayDigit(disp5, byte(digits[i5]));
-
-  if (pit == 5)
-    DisplayDigit(disp6, byte(dotDigits[i6]));
-  else
-    DisplayDigit(disp6, byte(digits[i6]));
-
-  if (pit == 6)
-    DisplayDigit(disp7, byte(dotDigits[i7]));
-  else
-    DisplayDigit(disp7, byte(digits[i7]));
-
-  if (pit == 7)
-    DisplayDigit(disp8, byte(dotDigits[i8]));
-  else
-    DisplayDigit(disp8, byte(digits[i8]));
+    if (pit < 4)
+      DisplayDigit(pit, byte(dotDigits[temp[pit]]), byte(digits[temp[pit + 4]]));
+    else
+      DisplayDigit(pit, byte(digits[temp[pit - 4]]), byte(dotDigits[temp[pit]]));
 }
 
 void CheckLevel()
 {
-  if (digitalRead(button2) == HIGH)
+  if (digitalRead(buttonTempUp) == HIGH)
   {
-    if (i2t == 10)
+    if (bTempUp == 10)
     {
-      switch (pit)
-      {
-      case 0:
-        i1++;
-        Beep(i1 * 20 + 200);
-        break;
-      case 1:
-        i2++;
-        Beep(i2 * 20 + 200);
-        break;
-      case 2:
-        i3++;
-        Beep(i3 * 20 + 200);
-        break;
-      case 3:
-        i4++;
-        Beep(i4 * 20 + 200);
-        break;
-      case 4:
-        i5++;
-        Beep(i5 * 20 + 200);
-        break;
-      case 5:
-        i6++;
-        Beep(i6 * 20 + 200);
-        break;
-      case 6:
-        i7++;
-        Beep(i7 * 20 + 200);
-        break;
-      case 7:
-        i8++;
-        Beep(i8 * 20 + 200);
-        break;
-      }
+      temp[pit]++;
       
-      i2t = 0;
+      if (temp[pit] > 9)
+        temp[pit] = 9;
+      else
+        Beep(temp[pit] * 20 + 200);
+      
+      bTempUp = 0;
     }
 
-    i2t++;
+    bTempUp++;
   }
 
-  if (analogRead(button2m) > 1000)
+  if (analogRead(buttonTempDown) > 1000)
   {
-    if (i2tm == 10)
+    if (bTempDown == 10)
     {
-      switch (pit)
-      {
-      case 0:
-        i1--;
-        Beep(i1 * 20 + 200);
-        break;
-      case 1:
-        i2--;
-        Beep(i2 * 20 + 200);
-        break;
-      case 2:
-        i3--;
-        Beep(i3 * 20 + 200);
-        break;
-      case 3:
-        i4--;
-        Beep(i4 * 20 + 200);
-        break;
-      case 4:
-        i5--;
-        Beep(i5 * 20 + 200);
-        break;
-      case 5:
-        i6--;
-        Beep(i6 * 20 + 200);
-        break;
-      case 6:
-        i7--;
-        Beep(i7 * 20 + 200);
-        break;
-      case 7:
-        i8--;
-        Beep(i8 * 20 + 200);
-        break;
-      }
+      temp[pit]--;
       
-      i2tm = 0;
+      if (temp[pit] < 0)
+        temp[pit] = 0;
+      else
+        Beep(temp[pit] * 20 + 200);
+      
+      bTempDown = 0;
     }
 
-    i2tm++;
+    bTempDown++;
   }
-
-  if (i1 > 9)
-    i1 = 9;
-  if (i2 > 9)
-    i2 = 9;
-  if (i3 > 9)
-    i3 = 9;
-  if (i4 > 9)
-    i4 = 9;
-  if (i5 > 9)
-    i5 = 9;
-  if (i6 > 9)
-    i6 = 9;
-  if (i7 > 9)
-    i7 = 9;
-  if (i8 > 9)
-    i8 = 9;
-
-  if (i1 <= 0)
-    i1 = 0;
-  if (i2 <= 0)
-    i2 = 0;
-  if (i3 <= 0)
-    i3 = 0;
-  if (i4 <= 0)
-    i4 = 0;
-  if (i5 <= 0)
-    i5 = 0;
-  if (i6 <= 0)
-    i6 = 0;
-  if (i7 <= 0)
-    i7 = 0;
-  if (i8 <= 0)
-    i8 = 0;
 }
 
 void Beep(int freq)
 {
-  noTone(beeper);
-  tone(beeper, freq, 250);
-}
-
-void Led()
-{
-  /*analogWrite(led1, (12 / 9 * i1));
-  analogWrite(led2, (12 / 9 * i2));
-  analogWrite(led3, (12 / 9 * i3));
-  analogWrite(led4, (12 / 9 * i4));*/
+  noTone(beeperPin);
+  tone(beeperPin, freq, 250);
 }
 
 void CheckOn()
 {
-  if (analogRead(button3) > 1000 && !on)
+  if (analogRead(buttonOn) > 1000 && !on)
   {
-    if (i3t == 5000)
+    if (bOn == 5000)
     {
       on = true;
       
@@ -363,27 +222,30 @@ void CheckOn()
       
       for (int i = 0; i < 500; i++)
       {
-        DisplayDigit(disp1, digits[0]);
-        DisplayDigit(disp2, B11101100);
+        DisplayDigit(0, digits[0], B00000000);
+        DisplayDigit(1, B11101100, B00000000);
+        DisplayDigit(2, B00000000, B00000000);
+        DisplayDigit(3, B00000000, B00000000);
       }
       
-      i3t = 0;
+      bOn = 0;
     }
 
-    i3t++;
+    bOn++;
   }
   
-  if (analogRead(button3m) > 1000 && on)
+  if (analogRead(buttonOff) > 1000 && on)
   {
-    if (i3tm == 50)
+    if (bOff == 50)
     {
       on = false;
       
       for (int i = 0; i < 500; i++)
       {
-        DisplayDigit(disp1, digits[0]);
-        DisplayDigit(disp2, digits[15]);
-        DisplayDigit(disp3, digits[15]);
+        DisplayDigit(0, digits[0], B00000000);
+        DisplayDigit(1, digits[15], B00000000);
+        DisplayDigit(2, digits[15], B00000000);
+        DisplayDigit(3, B00000000, B00000000);
       }
       
       DisplayOff();
@@ -396,40 +258,39 @@ void CheckOn()
       
       pit = 0;
       lock = 0;
-      i1 = 0;
-      i2 = 0;
-      i3 = 0;
-      i4 = 0;
       
-      i3tm = 0;
+      for (int i = 0; i < 8; i++)
+        temp[i] = 0;
+      
+      bOff = 0;
     }
 
-    i3tm++;
+    bOff++;
   }
 }
 
 void CheckPit()
 {
-  if (digitalRead(button1) == HIGH)
+  if (digitalRead(buttonPitUp) == HIGH)
   {
-    if (i1t == 30)
+    if (bPitUp == 30)
     {
-      pit++;
+      bPitUp++;
       
       if (pit > 7)
         pit = 0;
       
       Beep(440);
       
-      i1t = 0;
+      bPitUp = 0;
     }
 
-    i1t++;
+    bPitUp++;
   }
   
-  if (analogRead(button1m) > 1000)
+  if (analogRead(buttonPitDown) > 1000)
   {
-    if (i1tm == 30)
+    if (bPitDown == 30)
     {
       pit--;
       
@@ -438,10 +299,10 @@ void CheckPit()
       
       Beep(330);
       
-      i1tm = 0;
+      bPitDown = 0;
     }
-
-    i1tm++;
+    
+    bPitDown++;
   }
 }
 
@@ -449,9 +310,9 @@ int t = 0;
 
 void CheckLock()
 {
-  if (analogRead(button4) > 1000 && !lock)
+  if (analogRead(buttonSafeOn) > 1000 && !lock)
   {
-    if (i4t == 50)
+    if (bSafeOn == 50)
     {
       lock = true;
       
@@ -461,55 +322,55 @@ void CheckLock()
       
       for (int i = 0; i < 100; i++)
       {
-        DisplayDigit(disp1, dotDigits[5]);
-        DisplayDigit(disp2, dotDigits[10]);
-        DisplayDigit(disp3, dotDigits[15]);
-        DisplayDigit(disp4, dotDigits[14]);
+        DisplayDigit(0, digits[5], digits[0]);
+        DisplayDigit(1, digits[10], B11101100);
+        DisplayDigit(2, digits[15], B00000000);
+        DisplayDigit(3, digits[14], B00000000);
       }
       
-      i4t = 0;
+      bSafeOn = 0;
     }
 
-    i4t++;
+    bSafeOn++;
   }
   
-  if (analogRead(button4m) > 1000 && lock)
+  if (analogRead(buttonSafeOff) > 1000 && lock)
   {
-    if (i4tm == 100)
+    if (bSafeOff == 100)
     {
       lock = false;
       
       for (int i = 0; i < 100; i++)
       {
-        DisplayDigit(disp1, digits[5]);
-        DisplayDigit(disp2, digits[10]);
-        DisplayDigit(disp3, digits[15]);
-        DisplayDigit(disp4, digits[14]);
+        DisplayDigit(0, digits[5], digits[0]);
+        DisplayDigit(1, digits[10], digits[15]);
+        DisplayDigit(2, digits[15], digits[15]);
+        DisplayDigit(3, digits[14], B00000000);
       }
       
       Beep(330);
       delay(300);
       Beep(330);
       
-      i4tm = 0;
+      bSafeOff = 0;
     }
 
-    i4tm++;
+    bSafeOff++;
   }
 }
 
 void CheckBeep()
 {
-  if (digitalRead(button1) == HIGH || digitalRead(button2) == HIGH || analogRead(button3) > 1000 || analogRead(button4) > 1000 || analogRead(button1m) > 1000 || analogRead(button2m) > 1000 || analogRead(button3m) > 1000)
+  if (digitalRead(buttonPitUp) == HIGH || digitalRead(buttonTempUp) == HIGH || analogRead(buttonOn) > 1000 || analogRead(buttonSafeOn) > 1000 || analogRead(buttonTempDown) > 1000 || analogRead(buttonPitDown) > 1000 || analogRead(buttonOff) > 1000)
   {
     Beep(220);
     
     for (int i = 0; i < 50; i++)
     {
-        DisplayDigit(disp1, dotDigits[5]);
-        DisplayDigit(disp2, dotDigits[10]);
-        DisplayDigit(disp3, dotDigits[15]);
-        DisplayDigit(disp4, dotDigits[14]);
+        DisplayDigit(0, digits[5], digits[0]);
+        DisplayDigit(1, digits[10], digits[15]);
+        DisplayDigit(2, digits[15], B00000000);
+        DisplayDigit(3, digits[14], B00000000);
     }
   }
 }
